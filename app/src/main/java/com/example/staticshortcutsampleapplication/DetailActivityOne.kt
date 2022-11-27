c21ddaf53d6307309637921c366112529607b9a5
Made Wahyu Kharisma Candra Kirana <madewahyukharismacandrakirana@Mades-MacBook-Air.local>
2022-11-27 23:37:42 +0700

    first commit

---
+++ app/src/main/java/com/example/staticshortcutsampleapplication/DetailActivityOne.kt
@@ -0,0 +1,11 @@
+package com.example.staticshortcutsampleapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivityOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_one)
+    }
+}
---
+++ app/src/main/java/com/example/staticshortcutsampleapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.staticshortcutsampleapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.staticshortcutsampleapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
